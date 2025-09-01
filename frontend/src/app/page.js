1219c5724c91e48ba65d21322faa5fcb743d5240
Adarsh Agrahari <adarshagrahari0503@gmail.com>
2025-09-02 05:22:27 +0530

    updated

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -174,17 +174,17 @@ export default function HomePage() {
     }
 
     return (
-        <div className="px-10 py-6 max-w-7xl mx-auto">
+        <div className="md:px-10 md:py-6 max-w-7xl mx-auto">
             {/* Header */}
-            <div className="bg-white p-6 rounded-lg shadow mb-6 flex justify-between items-center">
+            <div className="bg-white p-4 md:p-6 rounded-lg shadow mb-6 flex flex-col justify-between items-center md:flex-row">
                 <div>
                     <h1 className="text-2xl font-bold">Products Management</h1>
                     <p className="text-gray-600">
                         Manage your product catalog and publishing status
                     </p>
                 </div>
-                <div className="flex gap-3">
-                    <button className="bg-green-600 text-white px-4 py-2 rounded-lg flex items-center gap-2 hover:bg-green-700">
+                <div className="flex gap-3 flex-col md:flex-row mt-4 w-full md:w-auto">
+                    <button className="bg-green-600 text-white px-4 py-2 rounded-lg flex justify-center items-center gap-2 hover:bg-green-700">
                         <Link href="/live">View Live Site</Link>
                     </button>
                     <button
